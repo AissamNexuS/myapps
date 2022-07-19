0d7ba961678146cbb891a371e7cd653e02f7f392
Aissam <ighirissam2001@gmail.com>
2022-07-19 15:32:11 +0200

    new app with login ans sign in

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Signup from './screens/Signup';
 import Signin from './screens/Signin';
 
+
 const Stack = createNativeStackNavigator();
 
 export default function app() {
