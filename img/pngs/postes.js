cc5754fb27b7ff6168705891d712abd3344307b4
AissamNexuS <108891852+AissamNexuS@users.noreply.github.com>
2022-07-20 15:50:30 +0200

    desian de home page

---
+++ img/pngs/postes.js
@@ -0,0 +1,43 @@
+const items = [
+    {
+        id: 1,
+        title: "post 1",
+        description: "Post 1 description",
+        date: "2022/07/20",
+        image: require('./4.png'),
+
+    },
+    {
+        id: 2,
+        title: "post 1",
+        description: "Post 1 description",
+        date: "2022/07/20",
+        image: require('./4.png'),
+
+    },
+    {
+        id: 3,
+        title: "post 1",
+        description: "Post 1 description",
+        date: "2022/07/20",
+        image: require('./4.png'),
+
+    },
+    {
+        id: 4,
+        title: "post 1",
+        description: "Post 1 description",
+        date: "2022/07/20",
+        image: require('./4.png'),
+
+    },
+    {
+        id: 5,
+        title: "post 1",
+        description: "Post 1 description",
+        date: "2022/07/20",
+        image: require('./4.png'),
+
+    },
+]
+export default items;
--- screens/Home.js
+++ screens/Home.js
@@ -1,23 +1,66 @@
-import { Text, View, StyleSheet, ScrollView, Image } from 'react-native'
+import { Text, View, StyleSheet, SafeAreaView, FlatList, ScrollView, Image, TouchableOpacity, StatusBar } from 'react-native'
 import React from 'react'
+import items from '../img/pngs/postes'
 
-const Home = () => {
+
+const Item = () => (
+    <View>
+        <Text style={contaire0.Tel}> Coca-Cola
+            <View >
+                <Image source={require("../img/pngs/3.png")} style={contaire0.logop3} />
+            </View><View >
+                <Image source={require("../img/pngs/coeur.png")} style={contaire0.logop4} />
+            </View>
+        </Text>
+        <View>
+            <Image source={require("../img/pngs/4.png")} style={{ width: 427.5, height: 285 }} />
+        </View>
+        <View>
+            <Text style={contaire0.Tel2}>Nouvelle proposition graphique</Text>
+            <Text style={contaire0.Tel3}>2021-08-31 — 09:39</Text>
+            <Text style={contaire0.Tel4}>Rabat, Agdal, Av Oqba ...</Text>
+        </View>
+    </View>
+);
+
+const Home = ({ navigation }) => {
+
+
+    const renderItem = ({ item }) => (
+        <Item
+            title={item.title}
+            description={item.description}
+            image={item.image}
+            date={item.date}
+        />
+
+    );
     return (
         <View style={contaire0.V}>
-            <ScrollView>
 
-                <Text style={contaire0.txt}>  Fil D'actualité
-                    <View >
+            <Text style={contaire0.txt}>  Fil D'actualité
+                <View >
+                    <TouchableOpacity >
                         <Image source={require("../img/pngs/2.png")} style={contaire0.logop1} />
-                    </View>
-                    <View >
+                    </TouchableOpacity>
+                </View>
+                <View >
+                    <TouchableOpacity onPress={() => { navigation.navigate("Signin") }}>
                         <Image source={require("../img/pngs/1.png")} style={contaire0.logop2} />
-                    </View>
-                </Text>
-
-                <Image source={require("../img/pngs/3.png")} style={contaire0.logop3} />
-            </ScrollView>
-        </View >
+                    </TouchableOpacity>
+                </View>
+            </Text>
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={items}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                />
+                <View style={contaire0.logop5}>
+                    <Image source={require("../img/pngs/5.png")} />
+                </View>
+            </SafeAreaView>
+        </View>
     )
 }
 
@@ -25,30 +68,68 @@ const contaire0 = StyleSheet.create({
     V: {
         flex: 1,
         padding: 10,
-        backgroundColor: '#FFFFFFFF'
+        backgroundColor: '#FFFFFFFF',
+
     },
     txt: {
         fontSize: 30,
         color: '#CA135E',
         fontWeight: 'bold',
-        marginVertical: 5,
+        left: 10,
     },
     logop1: {
         width: 34,
         height: 34,
         left: 90,
-        flexDirection: 'row-reverse'
     },
     logop2: {
         width: 34,
         height: 34,
         left: 130,
-        flexDirection: 'row-reverse'
     },
     logop3: {
         width: 49.92,
-        height: 48,
-        marginVertical: 20,
+        height: 40,
+        top: 12,
+        left: -160,
+    },
+    logop4: {
+        width: 34,
+        height: 34,
+        top: 12,
+        left: 160,
+    },
+    Tel: {
+        color: '#373737',
+        fontSize: 20,
+        fontWeight: '600',
+        left: 60,
+        height: 60,
+    },
+    Tel2: {
+        color: '#373737',
+        fontWeight: '600',
+        fontSize: 20,
+        left: 10,
+    },
+    Tel3: {
+        color: '#AAAAAA',
+        fontSize: 14,
+        left: 10,
+    },
+    Tel4: {
+        color: '#373737',
+        fontSize: 14,
+        left: 10
+    },
+    logop5: {
+        flexDirection: 'row-reverse',
+    },
+});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
     },
 });
 
